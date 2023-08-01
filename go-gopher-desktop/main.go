60e4a5fcb214b81c2e8690a40ba5c956c5963caa
mike <mikemonzo@gmail.com>
2023-08-01 16:58:47 +0200

    Add project go gopher desktop

---
+++ go-gopher-desktop/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+)
+
+const KuteGoAPIURL = "https://kutego-api-xxxxx.ew.a.run.app"
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Gopher")
+
+	//Main menu
+	fileMenu := fyne.NewMenu("File",
+		fyne.NewMenuItem("Quit", func() { myApp.Quit() }),
+	)
+
+	helpMenu := fyne.NewMenu("Help",
+		fyne.NewMenuItem("About", func() {
+			dialog.ShowCustom("About", "Close", container.NewVBox(
+				widget.NewLabel("Welcome to Gopher, a simple Desktop app created in Go with Fyne"),
+				widget.NewLabel("Version: v0.1"),
+				widget.NewLabel("Author: Miguel Monzo"),
+			), myWindow)
+		}))
+
+	mainMenu := fyne.NewMainMenu(fileMenu, helpMenu)
+	myWindow.SetMainMenu(mainMenu)
+
+	//Define a welcome text centered
+	text := canvas.NewText("Display a random Gopher!", color.White)
+	text.Alignment = fyne.TextAlignCenter
+
+	// Define a Gopher image
+	var resource, _ = fyne.LoadResourceFromURLString(KuteGoAPIURL + "/gopher/random/")
+	gopherImg := canvas.NewImageFromResource(resource)
+	gopherImg.SetMinSize(fyne.Size(500, 500)) // by default size is 0,0
+
+	// Define a "random" button
+	randomBtn := widget.NewButton("Random", func() {
+		resource, _ = fyne.LoadResourceFromURLString(KuteGoAPIURL + "/gopher/random/")
+		gopherImg.Resource = resource
+
+		// Redraw the image with the new path
+		gopherImg.Refresh()
+	})
+	randomBtn.Importance = widget.HighImportance
+
+	// Display a vertical box containing text, image and button
+	box := container.NewVBox(text, gopherImg, randomBtn)
+
+	// Display our content
+	myWindow.SetContent(box)
+
+	// Close the App when Escape key is pressed
+	myWindow.Canvas().SetOnTypedKey(func(k *fyne.KeyEvent) {
+		if k.Name == fyne.KeyEscape {
+			myApp.Quit()
+		}
+	})
+
+	// Show window and run app
+	myWindow.ShowAndRun()
+}
